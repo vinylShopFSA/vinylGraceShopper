ee4c442fe4311db4d8184b7da8aa1cf5ca1a9304
ida <ida.kamali10@gmail.com>
2022-10-25 16:21:44 -0400

    api route for single vinyl api

--- server/api/vinyls.js
+++ server/api/vinyls.js
@@ -4,9 +4,21 @@ const {
 } = require("../db");
 module.exports = router;
 
+//GET /api/vinyls
 router.get("/", async (req, res, next) => {
   try {
     const vinyls = await Vinyl.findAll();
+    // console.log(vinyls)
+    res.json(vinyls);
+  } catch (err) {
+    next(err);
+  }
+});
+
+//GET /api/vinyls/:id
+router.get("/:vinylId", async (req, res, next) => {
+  try {
+    const vinyls = await Vinyl.findByPk(req.params.vinylId);
     // console.log(vinyls)
     res.json(vinyls);
   } catch (err) {
