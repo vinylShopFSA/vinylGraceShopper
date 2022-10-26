cd5569b4b15edb3f6dee9f9d21bb75b24086b17f
ida <ida.kamali10@gmail.com>
2022-10-26 13:34:14 -0400

    add admin middleware

--- server/admin/index.js
+++ server/admin/index.js
@@ -1,10 +1,15 @@
 const router = require("express").Router();
 const { User } = require("../db/models");
 
+//admin middlewarefunction
+
 async function isUserAdmin(req, res, next) {
   const user = await User.findOne({ where: { id: req.user.id } });
   if (user.isAdmin == false) {
-    return res.status(401).json({ message: "Unauthorized" });
+    return res.status(403).json({ message: "Unauthorized" });
   }
   next();
 }
+
+//only admin can access all users
+router.get("/users");
