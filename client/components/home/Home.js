c4af96fa2e2f61481db145ae6e017c12475ba429
Orlando Caraballo <orlandocaraballo@gmail.com>
2022-10-26 12:30:22 -0400

    Added another note on folder structure

--- client/components/home/Home.js
+++ client/components/home/Home.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 
+// o: it looks like yall moved this into its own folder in the components
+//  folder instead of in the features folder. So the intention behind this was
+//  to have you all create a new folder per feature (vinyl) and have the component
+//  and any corresponding slices also in that folder
+
 /**
  * COMPONENT
  */
